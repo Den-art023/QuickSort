bc526c46eb1286ffde6e491a05aa7af7a45d4afa
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-10-19 17:00:25 +0700

    Add number of element in array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -12,6 +12,9 @@ namespace QuickSort
         private int[] arr = new int[20];
         private int cmp_count = 0; //number of comparison
         private int mov_count = 0; //Number of data movements
+
+        //Number of element in array
+        private int n;
         static void Main(string[] args)
         {
         }
