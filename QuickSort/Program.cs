20df76b6bf16651518266180ac86ed48651d0896
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-10-19 16:58:34 +0700

    Add private integer

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -8,7 +8,10 @@ namespace QuickSort
 {
     class Program
     {
-      
+        //Array of integers to holf values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparison
+        private int mov_count = 0; //Number of data movements
         static void Main(string[] args)
         {
         }
